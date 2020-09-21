817f7347bec9722d78f39f2b130ca3bf7d78c246
MaxHevchuk <maxhevchuk@gmail.com>
2020-09-21 22:20:15 +0300

    add projects: 1, 2, 3

--- 2/2.cs
+++ 2/2.cs
@@ -6,7 +6,32 @@ namespace _2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите вес: ");
+            float weight = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите рост: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Вы женаты? true or false: ");
+            bool isMarried = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("Введите дату: ");
+            object obj = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine($"Имя: {name}\n" +
+                $"Возраст: {age}\n" +
+                $"Вес: {weight}\n" +
+                $"Рост: {height}\n" +
+                $"Женаты: {isMarried}\n" +
+                $"Дата: {obj}");
+
+
+
+
         }
     }
 }
--- 3/3.cs
+++ 3/3.cs
@@ -6,7 +6,26 @@ namespace _3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Key = ");
+            bool key = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("Number: ");
+            uint number = Convert.ToUInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите пользователя: ");
+            while (Console.ReadLine() == "admin" && (key || number != 0))
+            {
+                Console.WriteLine("Введите число: ");
+                double doubleInput = Convert.ToDouble(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine("Hello World!");
+                    break;
+                } while (0.0 < doubleInput || doubleInput >= 10.0);
+            }
         }
     }
 }
